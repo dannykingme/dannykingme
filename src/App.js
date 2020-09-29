05e3c77ad248588156b133014f93088b2010fc43
Danny King <danny.newidea@gmail.com>
2020-09-29 16:05:12 -0500

    link components

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import React from 'react';
 import bkg from './bkg.jpg'; // eslint-disable-line no-unused-vars
 import './App.scss';
 
+const Link = ({ children, href, ...other }) => {
+  return <a href={href} {...other}>{children}</a>;
+}
+
+const LinkOut = ({ children, href, ...other }) => {
+  return <Link href={href} target="_blank" rel="noopener noreferrer" {...other}>{children}</Link>;
+}
+
 function App() {
   return (
     <div className="app">
-      <div class="background"></div>
+      <div className="background"></div>
       <section>
         <article>
           <h1>I'm Danny King.</h1>
           <p>
             <span>I design/develop tools for other designers/developers. Previously, I made prototyping simpler at </span>
-            <a href="https://precursorapp.com" target="_blank" rel="noopener noreferrer">Precursor</a>
+            <LinkOut href="https://precursorapp.com">Precursor</LinkOut>
             <span>. Currently, I'm making search simpler at </span>
-            <a href="https://google.com" target="_blank" rel="noopener noreferrer">Google</a>
+            <LinkOut href="https://google.com">Google</LinkOut>
             <span>. Eventually, I'll use more of </span>
-            <a href="https://twitter.com/dannykingme" target="_blank" rel="noopener noreferrer">Twitter</a>
+            <LinkOut href="https://twitter.com/dannykingme">Twitter</LinkOut>
             <span>, </span>
-            <a href="https://dribbble.com/dannykingme" target="_blank" rel="noopener noreferrer">dribbble</a>
+            <LinkOut href="https://dribbble.com/dannykingme">dribbble</LinkOut>
             <span>, & </span>
-            <a href="https://github.com/dannykingme" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <LinkOut href="https://github.com/dannykingme">GitHub</LinkOut>
             <span>.</span>
           </p>
         </article>
