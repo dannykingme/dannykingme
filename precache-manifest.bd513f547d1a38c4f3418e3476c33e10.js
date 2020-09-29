c82fbe88b1d50d9755e1edd2b6c07cefcdbd1552
Danny King <danny.newidea@gmail.com>
2020-09-29 15:34:09 -0500

    Updates

---
+++ precache-manifest.bd513f547d1a38c4f3418e3476c33e10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2fe1163d1c807bef64ea7c657e5a1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a772dc36ead17dbb82c",
+    "url": "/static/css/main.bedf7adf.chunk.css"
+  },
+  {
+    "revision": "f0a091784bdbd9d33cfc",
+    "url": "/static/js/2.6c3f528e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c3f528e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a772dc36ead17dbb82c",
+    "url": "/static/js/main.bd938c4f.chunk.js"
+  },
+  {
+    "revision": "b2ee3a9f265966f774e2",
+    "url": "/static/js/runtime-main.e0b4a8d2.js"
+  },
+  {
+    "revision": "f7575d2edf115771d0c230245a57b557",
+    "url": "/static/media/bkg.f7575d2e.jpg"
+  }
+]);
