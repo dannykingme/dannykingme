63acc1c339e76b8d92eb8302c22b72f4db40a483
Danny King <danny.newidea@gmail.com>
2020-09-30 21:49:05 -0500

    update url

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const Storyline = () => {
       <Story offset="5%" href="https://circleci.com/">Product Designer at CircleCI</Story> {/* aug 2013 */}
       <Story offset="15%" href="https://circleci.com/blog/adaptive-placeholders/">Invented Adaptive Placeholders</Story> {/* apr 2014 */}
       <Story offset="26%" href="https://precursorapp.com/">Founded Precursor</Story> {/* jan 2015 */}
-      <Story offset="33.5%" href="https://wearesumux.appspot.com/">UX Engineer at Google</Story> {/* oct 2015 */}
+      <Story offset="33.5%" href="https://design.google/sumux">UX Engineer at Google</Story> {/* oct 2015 */}
       <Story offset="43.5%" href="https://www.google.com/search?q=flowers">Shipped product to 2 billion users</Story> {/* jul 2016 */}
       <Story offset="53.5%" href="https://www.paygarden.com/">Head of Product at PayGarden</Story> {/* may 2017 */}
       <Story offset="59%" href="https://twitch.paygarden.com/">Created Twitch donation product</Story> {/* nov 2017 */}
