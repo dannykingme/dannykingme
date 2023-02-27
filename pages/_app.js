bd4fb3efd3bd2ba94d6f5c3449ed12f50b417f62
Danny King <danny.newidea@gmail.com>
2023-02-27 00:14:37 -0600

    nextjs app component

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/app.css';
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div className="app">
+      <Component {...pageProps} />
+    </div>
+  );
+}
