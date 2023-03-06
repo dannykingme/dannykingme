da9ef6d6f271446f9f159c4c991ddfb8a2943b19
Danny King <danny.newidea@gmail.com>
2023-03-06 02:20:35 -0600

    update precursor case study page

--- pages/precursor.js
+++ pages/precursor.js
@@ -2,6 +2,7 @@ import { useState, useRef } from 'react';
 
 import Link from 'next/link';
 import c from 'classnames';
+import { Footer, Header } from '../components/nav';
 
 const Precursor = () => {
   // const [state, setState] = useState(() => {
@@ -22,41 +23,12 @@ const Precursor = () => {
   return (
     <>
       <div className="main">
-        <div className="hero small">
-          <div className="header">
-            <div className="nav">
-              <div className="nav-items content">
-                <Link className="nav-item nav-brand" href="/portfolio">
-                  Danny King
-                </Link>
-                <Link className="nav-item" href="/about">
-                  About
-                </Link>
-                <Link className="nav-item" href="/press">
-                  Press
-                </Link>
-                <Link className="nav-item" href="/resume">
-                  Resume
-                </Link>
-                <Link className="nav-item" href="/contact">
-                  Contact
-                </Link>
-              </div>
-            </div>
-            {/* <div className="content">
-              <div className="header-item">DK</div>
-            </div> */}
+        <div className="hero small black">
+          <div className="hero-background">
+            <video src="precursor-broll-3.mp4" muted autoPlay loop />
+            <div className="hero-background-shade"></div>
           </div>
-          {/* <div className="hero-content content">
-            <div className="hero-title">Danny King</div>
-            <div className="hero-paragraph">
-              I build products for designers and developers. Here's my Twitter,
-              Dribbble, GitHub, and blog. I've also been featured in some press.
-            </div>
-            <div className="hero-action">
-              <button className="button">See work</button>
-            </div>
-          </div> */}
+          <Header />
           <div className="hero-content content">
             <div className="hero-case">
               <div className="hero-case-row">
@@ -88,7 +60,7 @@ const Precursor = () => {
         </div>
         <div className="article">
           <div className="content">
-            <div className="section">
+            {/* <div className="section">
               <div className="section-title">
                 <h1>Introduction</h1>
               </div>
@@ -96,17 +68,10 @@ const Precursor = () => {
                 <div className="section-dash"></div>
                 <h5>Background</h5>
               </div>
-            </div>
-            <p>
-              Precursor is a wireframing tool that helps people focus on their
-              work and collaborate with their team. I started the company in
-              2014 with my cofounder, Daniel Woelfel. After launch, Precursor
-              was featured as the #1 story on Hacker News, Product Hunt, and
-              Designer News, and reached over 100k new users in its first six
-              months with no marketing. In 2015 we decided to open-source the
-              product and have kept it running for ourselves and many other
-              users that are still active.
-            </p>
+            </div> */}
+            <h1>Introduction</h1>
+
+            <h4>Fixing collaboration for product teams</h4>
             <div className="media">
               <div className="media-video">
                 <video
@@ -118,6 +83,16 @@ const Precursor = () => {
               </div>
               <div className="media-caption">App Tour</div>
             </div>
+            <p className="space">
+              Precursor is a wireframing tool that helps people focus on their
+              work and collaborate with their team. I started the company in
+              2014 with my cofounder, Daniel Woelfel. After launch, Precursor
+              was featured as the #1 story on Hacker News, Product Hunt, and
+              Designer News, and reached over 100k new users in its first six
+              months with no marketing. In 2015 we decided to open-source the
+              product and have kept it running for ourselves and many other
+              users that are still active.
+            </p>
             {/* <div className="media">
               <div className="media-video">
                 <video
@@ -174,36 +149,42 @@ const Precursor = () => {
                 </div>
               </div>
             </div> */}
+          </div>
+          <div className="content-highlight">
+            <div className="content">
+              <h1>Challenge</h1>
 
-            <h1>Challenges</h1>
-            <h4>Escaping Complexity</h4>
-            <p>
-              Back in 2014, wireframing tools were not very easy to use or
-              collaborative compared to the tools available today. Most
-              prototyping was still done in high-end design tools like
-              Illustrator, or slightly lower fidelity tools like Balsamiq.
-              Neither worked well for me—Balsamiq still offered too many options
-              that distracted from the problem being solved, and the hand-drawn
-              aesthetic would trick my teammates into thinking this was a style
-              I settled on for the final product.
-            </p>
-            <p>
-              Precursor was one of the first real-time, multiplayer design
-              tools. Fast forward a decade, and these multiplayer features are
-              now very common in tools like Figma and Adobe XD.
-            </p>
-            <div className="media two">
-              <div className="media-image">
-                <img src="/precursor/precursor-before-1.png" alt="" />
-                <div className="media-caption">Balsamiq Interface</div>
+              <h4>Design tools chaos</h4>
+              <div className="media two">
+                <div className="media-image">
+                  <img src="/precursor/precursor-before-1.png" alt="" />
+                  <div className="media-caption">Balsamiq Interface</div>
+                </div>
+                <div className="media-image">
+                  <img src="/precursor/precursor-before-2.png" alt="" />
+                  <div className="media-caption">Illustrator Interface</div>
+                </div>
               </div>
-              <div className="media-image">
-                <img src="/precursor/precursor-before-2.png" alt="" />
-                <div className="media-caption">Illustrator Interface</div>
-              </div>
+              <p>
+                Back in 2014, wireframing tools were not very easy to use or
+                collaborative compared to the tools available today. Most
+                prototyping was still done in high-end design tools like
+                Illustrator, or slightly lower fidelity tools like Balsamiq.
+                Neither worked well for me—Balsamiq still offered too many
+                options that distracted from the problem being solved, and the
+                hand-drawn aesthetic would trick my teammates into thinking this
+                was a style I settled on for the final product.
+              </p>
+              <p>
+                Precursor was one of the first real-time, multiplayer design
+                tools. Fast forward a decade, and these multiplayer features are
+                now very common in tools like Figma and Adobe XD.
+              </p>
             </div>
+          </div>
 
-            <div className="section">
+          <div className="content">
+            {/* <div className="section">
               <div className="section-title">
                 <h1>Solutions</h1>
               </div>
@@ -211,20 +192,10 @@ const Precursor = () => {
                 <div className="section-dash"></div>
                 <h5>Production</h5>
               </div>
-            </div>
+            </div> */}
+            <h1>Solutions</h1>
 
-            <h4>Multiplayer Collaboration</h4>
-            <p>
-              Precursor was one of the first design tools that was built with
-              real-time, multiplayer features in-mind. Multiplayer design, as
-              opposed to collaboration design, allows all users to have a shared
-              sense of place. Not only were users able to manipulate each others
-              designs in real-time, but they could also see each other’s cursors
-              while changes were being made, and even chat via voice or text.
-              For most of our users this was their first time experiencing a
-              design process like that, and it really added to their sense of
-              teamwork, especially those working remotely.
-            </p>
+            <h4>Multiplayer collaboration</h4>
             <div className="media">
               <div className="media-video">
                 <video
@@ -236,8 +207,30 @@ const Precursor = () => {
               </div>
               <div className="media-caption">Multiplayer Collaboration</div>
             </div>
+            <p className="space">
+              Precursor was one of the first design tools that was built with
+              real-time, multiplayer features in-mind. Multiplayer design, as
+              opposed to collaboration design, allows all users to have a shared
+              sense of place. Not only were users able to manipulate each others
+              designs in real-time, but they could also see each other’s cursors
+              while changes were being made, and even chat via voice or text.
+              For most of our users this was their first time experiencing a
+              design process like that, and it really added to their sense of
+              teamwork, especially those working remotely.
+            </p>
 
-            <h4>Radial Menu</h4>
+            <h4>Radial menu</h4>
+            <div className="media">
+              <div className="media-video">
+                <video
+                  src="/precursor/radial-menu.mp4"
+                  muted
+                  autoPlay
+                  loop
+                ></video>
+              </div>
+              <div className="media-caption">Radial Menu</div>
+            </div>
             <p>
               Precursor’s appeal lies in its simplicity, and much of that
               simplicity is due to the intentional lack of distracting features;
@@ -250,7 +243,7 @@ const Precursor = () => {
               in a traditional toolbar, I wanted them as easy to access and swap
               between as possible.
             </p>
-            <p>
+            <p className="space">
               I liked the concept of radial menus after seeing them in video
               games, but while radial menus were somewhat common in video games
               back then, they were non-existent in the productivity/design
@@ -258,20 +251,20 @@ const Precursor = () => {
               popular design editor feature, making it’s way into other
               prototyping tools like Figma.
             </p>
+
+            <h4>Document replay</h4>
             <div className="media">
               <div className="media-video">
                 <video
-                  src="/precursor/radial-menu.mp4"
+                  src="/precursor/document-replay.mp4"
                   muted
                   autoPlay
                   loop
                 ></video>
               </div>
-              <div className="media-caption">Radial Menu</div>
+              <div className="media-caption">Document Replay</div>
             </div>
-
-            <h4>Document Replay</h4>
-            <p>
+            <p className="space">
               Precursor used a lot of cutting edge technology at the time of its
               launch including React, WebRTC, and immutable database structures.
               This led to some unique product features, including Replay. Every
@@ -284,84 +277,64 @@ const Precursor = () => {
               This is actually a feature I still use today, and it helped me
               build the hover animations on my portfolio’s homepage.
             </p>
-            <div className="media">
-              <div className="media-video">
-                <video
-                  src="/precursor/document-replay.mp4"
-                  muted
-                  autoPlay
-                  loop
-                ></video>
-              </div>
-              <div className="media-caption">Document Replay</div>
-            </div>
           </div>
         </div>
 
         <div className="hero small black">
           <div className="hero-background">
-            <video
-              ref={videoRef}
-              onCanPlay={() => setPlayBack()}
-              width="3840"
-              height="1970"
-              muted
-              autoPlay
-              loop
-            >
-              <source src="/test-vid2.mp4" type="video/mp4" />
-            </video>
+            <video src="precursor-broll-4.mp4" muted autoPlay loop />
             <div className="hero-background-shade"></div>
           </div>
           <div className="hero-content content">
             <div className="hero-stats">
               <h1>Results</h1>
-              {/* <div className="hero-case-row">
-                <div className="hero-case-cell">
-                  <div className="hero-case-title">Company</div>
-                  <div className="hero-case-subtitle">Netlify</div>
-                </div>
-              </div> */}
               <div className="hero-stats-row">
                 <div className="hero-stats-cell">
-                  <div className="hero-stats-title">Product Integrations</div>
-                  <div className="hero-stats-subtitle">{'>'}150</div>
-                  {/* <div className="hero-stats-subtitle">Integrations Hub</div> */}
+                  <div className="hero-stats-title">New users</div>
+                  <div className="hero-stats-subtitle">~200k</div>
                 </div>
                 <div className="hero-stats-cell">
-                  <div className="hero-stats-title">Pull Requests</div>
-                  <div className="hero-stats-subtitle">+ 134</div>
+                  <div className="hero-stats-title">Sign ups</div>
+                  <div className="hero-stats-subtitle">{'>'} 1,200</div>
                 </div>
                 <div className="hero-stats-cell">
-                  <div className="hero-stats-title">Lines of Code</div>
-                  <div className="hero-stats-subtitle">{'>'}60,000</div>
+                  <div className="hero-stats-title">Monthly growth</div>
+                  <div className="hero-stats-subtitle">+110%</div>
                 </div>
               </div>
               <div className="hero-stats-row">
                 <div className="hero-stats-cell">
-                  <div className="hero-stats-title">Onboarding</div>
+                  <div className="hero-stats-title">Popularity</div>
                   <div className="hero-stats-info">
-                    Netlify Graph's onboarding interaction was Netlify's first
-                    interactive onboarding user-experience.
+                    Precursor was feature as the #1 trending story on Hacker
+                    News, Product Hunt, and Designer News.
                   </div>
-                  {/* <div className="hero-stats-subtitle">Integrations Hub</div> */}
                 </div>
                 <div className="hero-stats-cell">
-                  <div className="hero-stats-title">Efficiency</div>
+                  <div className="hero-stats-title">Big teams</div>
                   <div className="hero-stats-info">
-                    Graph's five-year-old interface received a 90% rewrite and
-                    redesign in three months.
+                    Precursor was adopted by teams at Google, Facebook, Dropbox,
+                    AAA game studios, and ivy league schools.
+                  </div>
+                </div>
+                <div className="hero-stats-cell">
+                  <div className="hero-stats-title">Clojure success</div>
+                  <div className="hero-stats-info">
+                    Precursor was listed as one of Clojure's official{' '}
+                    <Link
+                      className="link"
+                      href="https://clojure.org/community/success_stories"
+                    >
+                      success stories
+                    </Link>
+                    .
                   </div>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="footer">
-            <div className="content">
-              <div className="footer-item">DK</div>
-            </div>
-          </div>
+          <Footer />
         </div>
       </div>
     </>
