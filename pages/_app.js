2f869209fa6337bee68be2c9fc681a343ebf9e80
Danny King <danny.newidea@gmail.com>
2023-03-06 20:18:45 -0600

    use google preconnect links in head to speed up font loads

--- pages/_app.js
+++ pages/_app.js
@@ -13,9 +13,11 @@ export default function MyApp({ Component, pageProps }) {
           content="initial-scale=1,maximum-scale=1"
           key="viewport"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Poppins:wght@400;600&family=Roboto+Condensed:wght@400;700&display=swap"
           rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto+Condensed:wght@400;700&family=Roboto+Mono&display=swap"
         />
       </Head>
       <Script
