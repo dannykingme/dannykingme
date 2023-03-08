62a8e8e276d56abe2d78fe7985c1998c5e0d5080
Danny King <danny.newidea@gmail.com>
2023-03-08 14:03:10 -0600

    fix compiler error

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ export default function MyApp({ Component, pageProps }) {
           key="viewport"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto+Condensed:wght@400;700&family=Roboto+Mono&display=swap"
